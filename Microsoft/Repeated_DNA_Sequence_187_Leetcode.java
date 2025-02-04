c2829ad7534bb221869bdcc2927cff20957f59a9
Pritish Biswas <pritish9801.edu@gmail.com>
2025-02-04 20:37:40 +0530

    added leetcode 187

---
+++ Microsoft/Repeated_DNA_Sequence_187_Leetcode.java
@@ -0,0 +1,19 @@
+package Microsoft;
+import java.util.*;
+
+
+public class Repeated_DNA_Sequence_187_Leetcode {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> seen = new HashSet() ; 
+        Set<String> repeated = new HashSet() ; 
+        for(int i = 0 ; i < s.length() - 9 ; i++) { 
+            String subStr = s.substring(i , i+ 10 ) ; 
+            if(seen.contains(subStr)) { 
+                repeated.add(subStr) ; 
+            }
+            seen.add(subStr) ; 
+
+        }
+        return new ArrayList(repeated) ; 
+    }
+}
