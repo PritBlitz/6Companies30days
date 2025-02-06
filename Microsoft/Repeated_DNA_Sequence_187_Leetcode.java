b81b1ca0ed71dcc2bed1ee20b13d7bc0b3f00a0e
Pritish Biswas <pritish9801.edu@gmail.com>
2025-02-06 19:06:52 +0530

    minor changes

--- Microsoft/Repeated_DNA_Sequence_187_Leetcode.java
+++ Microsoft/Repeated_DNA_Sequence_187_Leetcode.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class Repeated_DNA_Sequence_187_Leetcode {
     public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> seen = new HashSet() ; 
-        Set<String> repeated = new HashSet() ; 
+        Set<String> seen = new HashSet<>() ; 
+        Set<String> repeated = new HashSet<>() ; 
         for(int i = 0 ; i < s.length() - 9 ; i++) { 
             String subStr = s.substring(i , i+ 10 ) ; 
             if(seen.contains(subStr)) { 
@@ -14,6 +14,6 @@ public class Repeated_DNA_Sequence_187_Leetcode {
             seen.add(subStr) ; 
 
         }
-        return new ArrayList(repeated) ; 
+        return new ArrayList<>(repeated) ; 
     }
 }
